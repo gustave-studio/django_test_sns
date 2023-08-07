0ba0b0fe912d8d03c55390a0d83d01164dc9d2d0
gustave-studio <gustave0222@gmail.com>
2023-08-07 12:52:48 +0900

    ✨ログインしていない場合はログインページにリダイレクトする

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import signupfunc, loginfunc, listfunc
 
 urlpatterns = [
-    path('signup/', signupfunc),
-    path('login/', loginfunc),
-    path('list/', listfunc)
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    path('list/', listfunc, name='list')
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login
 from .models import BoardModel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -24,11 +25,12 @@ def loginfunc(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'login.html', {'context':'loged in'})
+            return redirect('list')
         else:
-            return render(request, 'login.html', {'context':'not loged in'})
-    return render(request, 'login.html', {'context':'get method'})
+            return render(request, 'login.html', {})
+    return render(request, 'login.html', {})
 
+# @login_required
 def listfunc(request):
     object_list = BoardModel.objects.all()
     return render(request, 'list.html', {'object_list':object_list})
