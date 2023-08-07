b0eaacd286fa90f5396cf852ed597a18b6e80791
gustave-studio <gustave0222@gmail.com>
2023-08-07 11:01:20 +0900

    🐛カラム名修正

--- boardapp/models.py
+++ boardapp/models.py
@@ -6,7 +6,7 @@ class BoardModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=50)
-    snsimages = models.ImageField(upload_to='')
+    snsimage = models.ImageField(upload_to='')
     good = models.IntegerField()
     read = models.IntegerField()
     readtext = models.TextField()
